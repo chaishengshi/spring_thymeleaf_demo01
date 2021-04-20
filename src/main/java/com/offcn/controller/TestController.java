aa00eb65a444bd3e02c4c4d7f0628d6183214100
chaishengshi <82867556+chaishengshi@users.noreply.github.com>
2021-04-20 18:16:21 +0800

    Update TestController.java
    修改方法

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -1,15 +1,16 @@
 package com.offcn.controller;
 
 /**
- * @Auther: lhq
- * @Date: 2020/12/4 15:13
+ * @Auther: css
+ * @Date: 
  * @Description:
  */
 public class TestController {
 
     public void test(){
         System.out.println("11111");
-        //update by lhq 
+        //update by css
+         System.out.println("22222");
         if(true){
             
         }
